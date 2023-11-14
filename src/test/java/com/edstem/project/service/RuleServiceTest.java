b0a6e9487e400543147280346ae6861c1eb98f09
Rexcy-Roy <rexcyroy135@gmail.com>
2023-11-14 15:36:45 +0530

    service testcase added

--- src/test/java/com/edstem/project/service/RuleServiceTest.java
+++ src/test/java/com/edstem/project/service/RuleServiceTest.java
@@ -1,81 +1,100 @@
-//package com.edstem.project.service;
-//
-//import com.edstem.project.contract.request.RuleRequest;
-//import com.edstem.project.contract.response.AllRuleResponse;
-//import com.edstem.project.contract.response.RuleResponse;
-//import com.edstem.project.model.Rule;
-//import com.edstem.project.repository.RuleRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//import org.modelmapper.ModelMapper;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class RuleServiceTest {
-//
-//    private RuleRepository ruleRepository;
-//    private ModelMapper modelMapper;
-//    private RuleService ruleService;
-//
-//    @BeforeEach
-//    public void init() {
-//        MockitoAnnotations.openMocks(this);
-//        ruleRepository = Mockito.mock(RuleRepository.class);
-//        modelMapper = new ModelMapper();
-//        ruleService =
-//                new RuleService(ruleRepository, modelMapper);
-//    }
-//
-//    @Test
-//    public void testCreateRule() {
-//        RuleRequest request = new RuleRequest();
-//        Rule rule = new Rule();
-//        request.setRule(rule);
-//
-//        when(ruleRepository.save(any(Rule.class))).thenReturn(rule);
-//
-//        RuleResponse response = ruleService.createRule(request);
-//
-//        verify(ruleRepository, times(1)).save(any(Rule.class));
-//        assertEquals("Success", response.getStatus());
-//        assertEquals("Rule created successfully", response.getMessage());
-//    }
-//
-//    @Test
-//    public void testCreateRuleException() {
-//        RuleRequest request = new RuleRequest();
-//        Rule rule = new Rule();
-//        request.setRule(rule);
-//
-//        when(ruleRepository.save(any(Rule.class))).thenThrow(new RuntimeException("Database error"));
-//
-//        RuleResponse response = ruleService.createRule(request);
-//
-//        verify(ruleRepository, times(1)).save(any(Rule.class));
-//        assertEquals("Error", response.getStatus());
-//        assertEquals("Failed to create the rule: Database error", response.getMessage());
-//    }
-//
-//    @Test
-//    public void testGetAllRules() {
-//        Rule rule1 = new Rule();
-//        Rule rule2 = new Rule();
-//        List<Rule> rules = Arrays.asList(rule1, rule2);
-//
-//        when(ruleRepository.findAll()).thenReturn(rules);
-//
-//        List<AllRuleResponse> responses = ruleService.getAllRules();
-//
-//        verify(ruleRepository, times(1)).findAll();
-//        assertEquals(rules.size(), responses.size());
-//    }
-//}
+package com.edstem.project.service;
+
+import com.edstem.project.contract.request.RuleAction;
+import com.edstem.project.contract.request.RuleCondition;
+import com.edstem.project.contract.request.RuleRequest;
+import com.edstem.project.contract.response.AllRuleResponse;
+import com.edstem.project.contract.response.RuleResponse;
+import com.edstem.project.model.Condition;
+import com.edstem.project.model.Rule;
+import com.edstem.project.repository.RuleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class RuleServiceTest {
+
+    private RuleRepository ruleRepository;
+    private ModelMapper modelMapper;
+    private RuleService ruleService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        ruleRepository = Mockito.mock(RuleRepository.class);
+        modelMapper = new ModelMapper();
+        ruleService =
+                new RuleService(ruleRepository, modelMapper);
+    }
+    @Test
+    void testCreateRule() {
+        Condition condition = new Condition();
+        condition.setClauses(new ArrayList<>());
+        condition.setId(1L);
+        condition.setType("Type");
+
+        Rule rule = new Rule();
+        rule.setActions(new ArrayList<>());
+        rule.setCondition(condition);
+        rule.setDescription("The characteristics of someone or something");
+        rule.setId(1L);
+        rule.setRuleId("42");
+        when(ruleRepository.save(Mockito.<Rule>any())).thenReturn(rule);
+
+        RuleAction ruleAction = new RuleAction();
+        ruleAction.setActionType("Error");
+        ruleAction.setActionValue(10.0d);
+
+        ArrayList<RuleAction> actions = new ArrayList<>();
+        actions.add(ruleAction);
+        RuleCondition conditionType = new RuleCondition();
+        conditionType.setClauses(new ArrayList<>());
+        conditionType.setType("Error");
+
+        RuleRequest request = new RuleRequest();
+        request.setConditionType(conditionType);
+        request.setActions(actions);
+        RuleResponse actualCreateRuleResult = ruleService.createRule(request);
+        verify(ruleRepository).save(Mockito.<Rule>any());
+        assertEquals("Rule created successfully", actualCreateRuleResult.getMessage());
+        assertEquals("Success", actualCreateRuleResult.getStatus());
+    }
+    @Test
+    void testDeleteRule() {
+        Condition condition = new Condition();
+        condition.setClauses(new ArrayList<>());
+        condition.setId(1L);
+        condition.setType("Type");
+        Rule rule = new Rule();
+        rule.setActions(new ArrayList<>());
+        rule.setCondition(condition);
+        rule.setDescription("The characteristics of someone or something");
+        rule.setId(1L);
+        rule.setRuleId("42");
+        Optional<Rule> ofResult = Optional.of(rule);
+        doNothing().when(ruleRepository).deleteById(Mockito.<Long>any());
+        when(ruleRepository.findById(Mockito.<Long>any())).thenReturn(ofResult);
+        ruleService.deleteRule(1L);
+        verify(ruleRepository).deleteById(Mockito.<Long>any());
+        verify(ruleRepository).findById(Mockito.<Long>any());
+    }
+    @Test
+    void testGetAllRules() {
+        when(ruleRepository.findAll()).thenReturn(new ArrayList<>());
+        List<AllRuleResponse> actualAllRules = ruleService.getAllRules();
+        verify(ruleRepository).findAll();
+        assertTrue(actualAllRules.isEmpty());
+    }
+}
