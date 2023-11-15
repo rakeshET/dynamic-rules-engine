f9d3f698424c49ca40399ee94dc7b7fb3896fee8
Anjalikprabhakar <anjali.k@edstem.com>
2023-11-15 12:39:53 +0530

    refactored code

--- src/main/java/com/edstem/project/contract/response/AllRuleResponse.java
+++ src/main/java/com/edstem/project/contract/response/AllRuleResponse.java
@@ -2,11 +2,8 @@ package com.edstem.project.contract.response;
 
 import com.edstem.project.contract.request.RuleAction;
 import com.edstem.project.contract.request.RuleCondition;
-import com.edstem.project.model.Action;
-import com.edstem.project.model.Rule;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 import lombok.Setter;
 
 import java.util.List;
@@ -15,7 +12,6 @@ import java.util.List;
 @Setter
 public class AllRuleResponse {
     private Long id;
-//    private String message;
     private String ruleId;
     private String description;
     private RuleCondition condition;
